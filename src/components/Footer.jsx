3d01bc318ca11d46ba34e7c1c1a98b8bba3549e2
ignacioOrellano <ignaciorellano@hotmail.com>
2025-02-19 20:53:08 -0300

    feat: ui footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="w-full h-8 box-border bg-stone-800 flex justify-center">
+      <span className="text-white text-[10px] self-center">
+        © 2025 Ignacio Orellano. All rights reserved.
+      </span>
+    </div>
+  );
+}
+
+export default Footer;
